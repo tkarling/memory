8e264aca4628b699a95abcdb88b589ea7e7ab74d
tkarling <tkarling@live.com>
2015-07-25 21:27:32 -0600

    user can see pokemon in cards
    user can see whether pair matched or not
    user can see winning text w time
    user can see count & timer in header

--- js/memoryController.js
+++ js/memoryController.js
@@ -1,16 +1,51 @@
 angular.module("myApp")
-.controller("memoryController", function($scope, $element, memoryService, webService) {
+.controller("memoryController", function($scope, $interval, memoryService, webService) {
 	$scope.test = "Controller Test";
 	$scope.wTest = webService.test;
 	$scope.mTest = memoryService.test;
-
 	$scope.table = memoryService.getTable();
 
+	// $scope.infoText = "";
+
+	$scope.matchCount = 0;
 	$scope.toggleSide = function(item) {
-		console.log(item);
-		// item.img = "https://cdn3.iconfinder.com/data/icons/meteocons/512/moon-symbol-128.png";
-		memoryService.toggleSide(item);
+		if($scope.infoText && ! $scope.infoText.includes("You Won")) {
+			$scope.infoText = "";
+		}
 		// console.log(item);
+		// item.img = "https://cdn3.iconfinder.com/data/icons/meteocons/512/moon-symbol-128.png";
+		var matchFound = memoryService.toggleSide(item);
+		// console.log('matchFound', matchFound);
+		if(matchFound) {
+			$scope.infoText = "Match Found!";
+			$scope.matchCount++;
+			if($scope.matchCount === 8) { // KORJATTU
+				var minAndSecs = $scope.time.split(":");
+				// $scope.infoText = "You Won in " + minAndSecs[0] + " mins " + minAndSecs[1] + " seconds!!"; 
+				$scope.infoText = "You Won in " + minsSecsTimeString() + "!!"; 
+				$interval.cancel(myInterval);
+			}
+		}
 	};
 
+	var colonTimeString = function() {
+		var displaySeconds = seconds % 60;
+		var secondsStr =  (displaySeconds < 10) ? "0" + displaySeconds: displaySeconds;
+		return Math.floor(seconds / 60).toFixed() + ":" + secondsStr;
+	};
+
+	var minsSecsTimeString = function () {
+		var mins = Math.floor(seconds / 60);
+		var minStr = (mins > 0) ? Math.floor(seconds / 60).toFixed() + " mins and " : "";
+		return minStr + seconds % 60 + " seconds";
+	};
+
+	var seconds = 0;
+	$scope.time = "0.00"
+	var myInterval = $interval(function() {
+		++seconds;
+		$scope.time = colonTimeString(seconds);
+	}, 1000, 60*60);
+
+	
 });
--- js/memoryService.js
+++ js/memoryService.js
@@ -1,25 +1,32 @@
 angular.module("myApp")
     .service("memoryService", function() {
-        console.log("init memoryService");
+        // console.log("init memoryService");
         this.test = "Memory Service Test"
 
         var backPic = "https://cdn0.iconfinder.com/data/icons/good-weather-1/96/weather_icons-01-128.png";
         var frontPic = "https://cdn3.iconfinder.com/data/icons/meteocons/512/moon-symbol-128.png";
+        // e.g. http://assets22.pokemon.com/assets/cms2/img/pokedex/full/002.png
+        var frontUrl = "http://assets22.pokemon.com/assets/cms2/img/pokedex/full/00"
         var table = [];
         var cols = 4;
         var rows = 4;
 
         var initTable = function() {
+        	var pokemonId = 0;
             for (var i = 0; i < rows; i++) {
                 table[i] = {
                     row: []
                 };
                 for (var j = 0; j < cols; j++) {
                     var str = i + " " + j;
+                    pokemonId = pokemonId % 8 + 1;
+                    // console.log('pokemonId', pokemonId);
                     table[i].row.push({
                         text: str,
+                        pokemonId: pokemonId,
                         img: backPic,
-                        imgFront: frontPic
+                        // imgFront: "https://cdn3.iconfinder.com/data/icons/meteocons/512/moon-symbol-128.png"
+                        imgFront: frontUrl + pokemonId + ".png"
                     });
                 }
             }
@@ -29,29 +36,41 @@ angular.module("myApp")
             return table;
         };
 
+        var matchFound = function() {
+        	return openItems[0].pokemonId === openItems[1].pokemonId;
+        }
+
         var openItems = [];
         var itemsOpen = 0;
-        this.toggleSide = function (item) {
-        	if (itemsOpen >= 2) {
-        		for(var i = 0; i < openItems.length; i++) {
-        			openItems[i].img = backPic;
-        		}
-        		openItems = [];
-       			itemsOpen = 0;
-        	} else if(item.img === backPic) {
-        		if(itemsOpen === 0) {
-        			openItems[0] = item;
-        		} else if(itemsOpen === 1) {
-        			openItems[1] = item;
-        		}
-        		if(itemsOpen < 2) {
-	        		itemsOpen++;
-					item.img = item.imgFront;
-        		}
-        	} 
+        this.toggleSide = function(item) {
+            if (itemsOpen >= 2) {
+                for (var i = 0; i < openItems.length; i++) {
+                	if(matchFound()) {
+	                    openItems[i].img = frontPic;
+                	} else {
+	                    openItems[i].img = backPic;
+                	}
+                }
+                openItems = [];
+                itemsOpen = 0;
+            } else if (item.img === backPic) {
+                if (itemsOpen === 0) {
+                    openItems[0] = item;
+                } else if (itemsOpen === 1) {
+                    openItems[1] = item;
+                }
+                if (itemsOpen < 2) {
+                    itemsOpen++;
+                    item.img = item.imgFront;
+                }
+                if(itemsOpen === 2) {
+                	return matchFound();
+                }
+            }
+            return false;
         }
 
         initTable();
-        console.log(table);
+        // console.log(table);
 
     });
